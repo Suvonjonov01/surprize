6c97ede37c089681af08fc88779719f52ed1ee0a
Suvonjonov01 <147715884+Suvonjonov01@users.noreply.github.com>
2023-12-06 01:01:57 +0800

    l
    l

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function hiddenBlock() {
+
+	document.getElementById("img").style = ("display:block; position:absolute;");
+
+	document.getElementById("btn").style = ("display:none");
+
+	document.getElementById("audio").style = ("display:none; position:absolute; top: 5%; left: 40%;");
+
+	document.getElementById("audio").play();
+}
+
